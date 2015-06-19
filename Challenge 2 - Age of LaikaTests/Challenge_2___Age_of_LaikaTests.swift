9125b2cf3400447c91e6eebf2ccd551394c36403
Maxence De Damas <MD@MacBook.local>
2015-06-19 09:21:30 -0300

    Initial Commit

---
+++ Challenge 2 - Age of LaikaTests/Challenge_2___Age_of_LaikaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Challenge_2___Age_of_LaikaTests.swift
+//  Challenge 2 - Age of LaikaTests
+//
+//  Created by Maxence De Damas on 6/19/15.
+//  Copyright (c) 2015 Maxence de Damas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Challenge_2___Age_of_LaikaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
