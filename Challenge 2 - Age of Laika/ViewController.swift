58cbe26aa7d7c4cbb1f3babdfe63e7ca214b47a8
Maxence De Damas <MD@MacBook.local>
2015-06-19 10:14:36 -0300

    first commit

--- Challenge 2 - Age of Laika/ViewController.swift
+++ Challenge 2 - Age of Laika/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsConvertedLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertHumanToDogYearsButtonPressed(sender: UIButton) {
+        let sizeFromTextField = enterHumanYearsTextField.text.toInt()!
+        let conversionConstant = 7
+        
+        dogYearsConvertedLabel.hidden = false
+        dogYearsConvertedLabel.text = "\(sizeFromTextField * conversionConstant) in Dog Years"
+        dogYearsConvertedLabel.textColor = UIColor.blueColor()
+       
+        enterHumanYearsTextField.text = ""
+        enterHumanYearsTextField.resignFirstResponder()
+        
+        convertButton.setTitle("Wof Wof", forState: UIControlState.Normal)
+        convertButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        
+        
+        
+    }
 
 }
 
